f26f8b5cd7317673802df44a24df402e55bda9ed
unknown <kadirovbrucelee@gmail.com>
2023-10-12 18:07:33 +0500

    update feedback block

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
     const swiper = new Swiper('.popkorn_swiper', {
+      loop: true,
       spaceBetween: 30,
-      slidesPerView: 6,
+      slidesPerView: 1,
       autoplay: {
         delay: 300,
         disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     });
 
     const swiper_2 = new Swiper('.feeback_swiper', {
       loop: true,
-      slidesPerView: 4 ,
+      slidesPerView: 1 ,
       spaceBetween:30 , 
       // Navigation arrows
       navigation: {
