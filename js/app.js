220e54bdae2e33eb948dd39512efe2b6c8a277b4
unknown <kadirovbrucelee@gmail.com>
2023-10-13 22:47:45 +0500

    adaptive change

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     const swiper = new Swiper('.popkorn_swiper', {
       loop: true,
       spaceBetween: 30,
-      slidesPerView: 1,
       autoplay: {
         delay: 300,
         disableOnInteraction: false,
@@ -11,6 +10,24 @@ $(document).ready(function(){
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
+      breakpoints:{
+        320:{
+          slidesPerView: 1,
+        },
+        500:{
+          slidesPerView: 2,
+        },
+        768:{
+          slidesPerView: 3,
+        },
+        900:{
+          slidesPerView: 4,
+        },
+        1200:{
+          slidesPerView: 5,
+        },
+       
+      }
     });
 
     const swiper_2 = new Swiper('.feeback_swiper', {
@@ -24,3 +41,9 @@ $(document).ready(function(){
       },
     });
 })
+// kwork 
+const burgerBtn= document.querySelector('.burger-btn');
+const headerRow= document.querySelector('.header_row');
+burgerBtn.onclick = () => {
+  headerRow.classList.toggle('header-show');
+}
